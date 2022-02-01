93450ed1741e90baa89cd58fe5a30deec142fad2
TheMarcusDev <marcushen.j@gmail.com>
2022-02-01 19:45:29 -0300

    if else

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,26 @@
 var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 
-function increment() {
+function increment() {    
     currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    currentNumberWrapper.innerHTML = currentNumber;   
 }
 
 function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }   
+
+if (currentNumber >= 10) {
+    document.getElementById("buttonadd").disabled=true;
+}
+    else {
+        document.getElementById("buttonadd").disabled=false;
+}
+
+if (currentNumber <= 0) {
+    document.getElementById("buttonsub").disabled=true;
+}
+    else {
+        document.getElementById("buttonsub").disabled=false;
+}
